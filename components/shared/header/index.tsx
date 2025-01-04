d278fb42764409f1573b451ff09aa02b2c3e05d8
Artem Kovtun <rtmkvtn@gmail.com>
2025-01-04 13:11:08 +0800

    feat: mobile header menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import {ShoppingCart, UserIcon} from 'lucide-react'
 import Image from 'next/image'
 import {APP_NAME} from "@/lib/constants";
 import Link from "next/link";
-import {Button} from "@/components/ui/button";
-import ModeToggle from '@/components/shared/header/mode-toggle'
+import Menu from './menu'
 
 const Header = () => {
   return (
@@ -15,20 +13,7 @@ const Header = () => {
             <span className="hidden lg:block font-bold text-2xl ml-3">{APP_NAME}</span>
           </Link>
         </div>
-
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   )
