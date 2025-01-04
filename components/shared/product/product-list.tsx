c77ba4a80534f51f74a45377e7bd18c4d1decc4a
Artem Kovtun <rtmkvtn@gmail.com>
2025-01-04 14:48:16 +0800

    feat: ProductCard component

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,4 @@
+import ProductCard from '@/components/shared/product/product-card'
 
 const ProductList = ({ data, title, limit }: { data: any, title?: string, limit?: number }) => {
   const limitedData = limit ? data.slice(0, limit) : data
@@ -7,13 +8,13 @@ const ProductList = ({ data, title, limit }: { data: any, title?: string, limit?
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: any, i: number) => (
-            <div key={`product-in-a-list-${i}`}>{product.name}</div>
+          {limitedData.map((product: any) => (
+            <ProductCard key={product.slug} product={product} />
           ))}
         </div>
       ) : (
         <div>
-        <p>No productss found</p>
+        <p>No products found</p>
         </div>
         )}
     </div>
