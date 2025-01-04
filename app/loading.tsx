bd4ad73a81d1d74c6024906dc242ec57f8c6d586
Artem Kovtun <rtmkvtn@gmail.com>
2025-01-04 13:01:55 +0800

    feat: loading page; not found page

---
+++ app/loading.tsx
@@ -0,0 +1,17 @@
+import loader from '@/assets/loader.gif'
+import Image from 'next/image'
+
+const LoadingPage = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      width: '100vw',
+    }}>
+      <Image src={loader} alt="Loader..." height={150} width={150} />
+    </div>
+  )
+}
+export default LoadingPage
