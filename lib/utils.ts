409c65e8d37b74f5b74da7b318fd94f04c8804b8
Artem Kovtun <rtmkvtn@gmail.com>
2025-02-04 08:50:06 +0800

    feat: add cart page

--- lib/utils.ts
+++ lib/utils.ts
@@ -48,3 +48,20 @@ export function round2(value: number | string) {
     return Math.round((Number(value) + Number.EPSILON) * 100) / 100
   }
 }
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
+  currency: 'USD',
+  style: 'currency',
+  minimumFractionDigits: 2,
+})
+
+// Format currency using the formatter above
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === 'number') {
+    return CURRENCY_FORMATTER.format(amount)
+  } else if (typeof amount === 'string') {
+    return CURRENCY_FORMATTER.format(Number(amount))
+  } else {
+    return 'NaN'
+  }
+}
