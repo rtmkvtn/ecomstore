ed49719a5bf107004150aab840c1787b11dfc53f
Artem Kovtun <rtmkvtn@gmail.com>
2025-03-19 09:50:28 +0800

    feat: add functionality to retrieve all products and delete a product
    - Implemented `getAllProducts` function to retrieve a paginated list of products.
    - Added `deleteProduct` function to remove a product by ID with error handling and path revalidation.

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
+
 import { prisma } from '@/db/prisma'
-import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants'
-import { convertToPlainObject } from '@/lib/utils'
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '@/lib/constants'
+import { convertToPlainObject, formatError } from '@/lib/utils'
 
 // Get latest products
 export async function getLatestProducts() {
@@ -21,4 +23,44 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   })
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string
+  limit?: number
+  page: number
+  category?: string
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  })
+
+  const dataCount = await prisma.product.count()
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  }
+}
+
+// Delete a product
+export async function deleteProduct(id: string) {
+  try {
+    const productExists = await prisma.product.findFirst({ where: { id } })
+    if (!productExists) throw new Error(`Product is not found`)
+
+    await prisma.product.delete({ where: { id } })
+    revalidatePath('/admin/products')
+
+    return { success: true, message: 'Product deleted successfully.' }
+  } catch (e) {
+    return { success: false, message: formatError(e) }
+  }
 }
